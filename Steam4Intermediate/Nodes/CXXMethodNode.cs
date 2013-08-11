0b2cd351b11fe24efdfc36b40791a5c9d59cc521
Ryan Kistner <azuisleet@gmail.com>
2013-08-11 17:16:56 -0600

    Initial commit of refactored Steam4NET: - Removes autogenerated code in favor of interface declarations - Passes current test suite - Removed parameter masking for StringBuilder types

---
+++ Steam4Intermediate/Nodes/CXXMethodNode.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Xml;
+using Steam4Intermediate.NodeBehavior;
+using System.Text;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Steam4Intermediate.Nodes
+{
+    class CXXMethodNode : LinkBehavior
+    {
+        public CXXMethodNode(XmlAttributeCollection collection)
+            : base(collection)
+        {
+
+        }
+
+        public override void EmitCode(Generator generator, int depth, int ident)
+        {
+            bool genericwrapper = false;
+            List<int> maskedparams = new List<int>();
+
+            INode returntype;
+            bool constness, pointer;
+
+            string returns_base = base.ResolveType( 0, out returntype, out constness, out pointer );
+
+            Debug.Assert(returns_base != null);
+
+            if ( returns_base == null )
+            {
+                returns_base = "(null)";
+            }
+
+            string returns = returns_base;
+
+            if ( pointer )
+            {
+                if ((returntype is RecordNode && returntype.GetAttribute("kind") == "class" && returntype.GetName().StartsWith("I") ) ||
+                        (returntype is FundamentalTypeNode && returns == "void"))
+                {
+                    genericwrapper = true;
+                    returns = "IntPtr";
+                }
+            }
+
+            returns = generator.ResolveType( returns, constness, pointer, true, false );
+
+            /*if (returns == "UInt64" || returns == "Int64")
+            {
+                returnbystack = true;
+            }
+            */
+
+            List<INode> args = new List<INode>();
+
+            foreach( INode child in children )
+            {
+                if ( child is ParmVarNode )
+                {
+                    args.Add( child );
+                }
+            }
+
+            List<string> arg = new List<string>();
+            List<string> arg_native = new List<string>();
+            List<string> arg_native_pure = new List<string>();
+
+            arg_native.Add( "IntPtr thisptr" );
+
+            for (int i = 0; i < args.Count; i++)
+            {
+                INode argtype;
+                bool argconst, argpointer;
+
+                string argname = args[i].GetName();
+
+                string argtypes = args[i].ResolveType(0, out argtype, out argconst, out argpointer);
+
+                Debug.Assert(argtypes != null);
+
+                argtypes = generator.ResolveType(argtypes, argconst, argpointer, false, true);
+
+                if ( genericwrapper && argtypes == "string" )
+                {
+                    maskedparams.Add( i );
+                }
+
+                if ( String.IsNullOrEmpty( argname ) )
+                {
+                    argname = "arg" + i;
+                }
+
+                if ( !maskedparams.Contains( i ) )
+                    arg.Add( argtypes + " " + argname );
+            }
+
+            string methodname = GetName();
+            string extra = "";
+
+            if ( genericwrapper )
+            {
+                returns = "TClass";
+                methodname = methodname + "<TClass>";
+                extra = " where TClass : class";
+            }
+
+            generator.EmitLine( String.Format( "[VTableSlot({0})]", ident ), depth );
+            generator.EmitLine( String.Format( "{0} {1}({2}){3};", returns, methodname, String.Join(", ", arg.ToArray() ), extra), depth );
+        }
+
+
+        private string GetArgIdent( List<string> args )
+        {
+            StringBuilder ident = new StringBuilder();
+
+            foreach( string arg in args )
+            {
+                //ident.Append( arg.Substring(0, 1).ToUpper() );
+        
+                ident.Append( arg.Replace("ref", "").Trim().Substring(0, 1).ToUpper() );
+            }
+
+            return ident.ToString();
+        }
+
+    }
+}
---
+++ Steam4NET/Core/InteropHelp.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Reflection;
+
+namespace Steam4NET.Core
+{
+    public class InteropHelp
+    {
+
+        /// <summary>
+        /// Decodes ANSI encoded return string to UTF-8
+        /// </summary>
+        public static string DecodeANSIReturn(string buffer)
+        {
+            return Encoding.UTF8.GetString(Encoding.Default.GetBytes(buffer));
+        }
+
+        public class BitVector64
+        {
+            private UInt64 data;
+
+            public BitVector64()
+            {
+            }
+            public BitVector64(UInt64 value)
+            {
+                data = value;
+            }
+
+            public UInt64 Data
+            {
+                get { return data; }
+                set { data = value; }
+            }
+
+            public UInt64 this[uint bitoffset, UInt64 valuemask]
+            {
+                get
+                {
+                    return (data >> (ushort)bitoffset) & valuemask;
+                }
+                set
+                {
+                    data = (data & ~(valuemask << (ushort)bitoffset)) | ((value & valuemask) << (ushort)bitoffset);
+                }
+            }
+        }
+    }
+}
